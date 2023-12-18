2cbe56b4f54b74a58952a627998ac313c1e7ec2e
patricksiandji <patrick.djumedio@pcg.io>
2023-12-18 14:18:02 +0100

    remove addition route

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-
-@app.get("/addition")
-def addition(x:float, y:float):
-    return x + y
-
 @app.get("/difference")
 def addition(x:float, y:float):
     return x - y
