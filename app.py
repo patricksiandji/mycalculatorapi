ca20f03b15e26035e1ab343270525dc295a826f7
patricksiandji <patrick.djumedio@pcg.io>
2023-12-18 15:29:05 +0100

    updated main

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/addition")
+def addition(x:float, y:float):
+    return x + y
+
+@app.get("/difference")
+def addition(x:float, y:float):
+    return x - y
+
 @app.get("/multiplication")
 def addition(x:float, y:float):
     return x * y
