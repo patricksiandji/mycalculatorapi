7a4a2212b717f23ee26df8004ac2cc85a9c199f0
patricksiandji <patrick.djumedio@pcg.io>
2023-12-19 15:36:33 +0100

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,39 @@
 
-variable "project_id" {
-  type    = string
-  default = "cw-patrick-sandbox"
+# variable "project_id" {
+#   type    = string
+#   default = "cw-patrick-sandbox"
 
-}
+# }
 
-variable "default_region" {
-  type    = string
-  default = "europe-west1" #belgium
-}
+# variable "default_region" {
+#   type    = string
+#   default = "europe-west1" #belgium
+# }
 
-variable "default_zone" {
-  type    = string
-  default = "europe-west1-b"
+# variable "default_zone" {
+#   type    = string
+#   default = "europe-west1-b"
 
-}
+# }
 
-variable "cr_app_name" {
-  type    = string
-}
+# variable "cr_app_name" {
+#   type    = string
+# }
 
-variable "cr_image" {
-  type    = string
-}
+# variable "cr_image" {
+#   type    = string
+# }
 
-variable "container_port" {
-  type = number
+# variable "container_port" {
+#   type = number
 
-}
-variable "srv_account_id" {
-  type    = string
+# }
+# variable "srv_account_id" {
+#   type    = string
 
-}
+# }
 
-variable "service_port" {
-  type    = string
-  default = 80
-}
+# variable "service_port" {
+#   type    = string
+#   default = 80
+# }
