ce112083f18a342d2b21e0a89d623168b66effbd
patricksiandji <patrick.djumedio@pcg.io>
2023-12-19 10:24:46 +0100

    added terraform registry and update .gitignore

--- registry.tf
+++ registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "repo_nginx_from_tf" {
       location      = "europe-west1"
-      repository_id = "ci_cd_images"
+      repository_id = "ci-cd-images"
       description   = "fastapi repository"
       format        = "DOCKER"
  }
