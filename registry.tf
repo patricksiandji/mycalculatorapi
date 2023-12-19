1905e596c145e175c82d58e1881a162373fcd644
patricksiandji <patrick.djumedio@pcg.io>
2023-12-19 12:41:07 +0100

    update registry name

--- registry.tf
+++ registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "repo_nginx_from_tf" {
       location      = "europe-west1"
-      repository_id = "ci-cd-images"
+      repository_id = "ci-cd-from-tf"
       description   = "fastapi repository"
       format        = "DOCKER"
  }
