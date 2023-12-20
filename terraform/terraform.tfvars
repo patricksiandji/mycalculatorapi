79c1004d0ffefb1ba882c2d2df379b67b853549c
patricksiandji <patrick.djumedio@pcg.io>
2023-12-20 08:50:19 +0100

    add a github workflow

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+cr_app_name = "fastapi-srv"
+default_region = "europe-west1"
+default_zone = "europe-west1-b"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,43 @@
 
-# variable "project_id" {
-#   type    = string
-#   default = "cw-patrick-sandbox"
+variable "project_id" {
+  type    = string
+  default = "cw-patrick-sandbox"
 
-# }
+}
 
-# variable "default_region" {
-#   type    = string
-#   default = "europe-west1" #belgium
-# }
+variable "default_region" {
+  type    = string
+  default = "europe-west1" #belgium
+}
 
-# variable "default_zone" {
-#   type    = string
-#   default = "europe-west1-b"
+variable "default_zone" {
+  type    = string
+  default = "europe-west1-b"
 
-# }
+}
 
-# variable "cr_app_name" {
-#   type    = string
-# }
+variable "cr_app_name" {
+  type    = string
+  default = "cr-fastapi"
+}
 
-# variable "cr_image" {
-#   type    = string
-# }
+variable "cr_image" {
+  type    = string
+  default = "fastapi"
+}
 
-# variable "container_port" {
-#   type = number
+variable "container_port" {
+  type = number
+  default = 80
 
-# }
-# variable "srv_account_id" {
-#   type    = string
+}
+variable "srv_account_id" {
+  type    = string
+  default = "my-srv-acc"
 
-# }
+}
 
-# variable "service_port" {
-#   type    = string
-#   default = 80
-# }
+variable "service_port" {
+  type    = string
+  default = 80
+}
